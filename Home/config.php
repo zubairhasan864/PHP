0e64cbc37d9edb30ffec6aea370da80fb402b143
zubairhasan864 <jarakikenpachi586@gmail.com>
2025-04-03 12:33:52 +0600

    Create config.php

---
+++ Home/config.php
@@ -0,0 +1,9 @@
+<?php
+define("Config", "PHP  App");
+define("BASE_URL", "http://localhost/php-app");
+
+echo "<h2>Config Loaded</h2>";
+echo "<p>Application Name: "Config page. "</p>";
+echo "<p>Base URL: " . BASE_URL . "</p>";
+?>
+<a href="index.php">Go Back</a>
